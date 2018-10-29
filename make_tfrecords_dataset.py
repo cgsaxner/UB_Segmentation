979564e559d1d240921066bb89e8b52116562b13
cgsaxner <christina.gsaxner@gmail.com>
2018-10-29 10:50:04 +0100

    Update make_tfrecords_dataset.py

--- make_tfrecords_dataset.py
+++ make_tfrecords_dataset.py
@@ -1,12 +1,11 @@
 import tf_records
 import os
 
-data_path = "F:/TestingDataNoAug"
-images_path = os.path.join(data_path, "Images")
-labels_path = os.path.join(data_path, "Labels")
-
+data_path = "PATH_TO_IMAGE_DATA"
+tfrecords_filename = os.path.join(data_path, "Data.tfrecords")
 height, width = 512, 512
 
-tfrecords_filename = os.path.join(data_path, "TestingData_512.tfrecords")
+images_path = os.path.join(data_path, "Images")
+labels_path = os.path.join(data_path, "Labels")
 
 tf_records.write_to_tfrecords(images_path, labels_path, tfrecords_filename, height, width)
